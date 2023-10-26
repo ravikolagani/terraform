004c6fb3fcb64aaabfb53aa827963b450745e5fe
ravikolagani <ravi.kolagani@gmail.com>
2023-10-27 07:43:23 +0800

    Added ec2 resource and modified to add name in tags

--- practice/first_ec2.tf
+++ practice/first_ec2.tf
@@ -3,3 +3,10 @@ provider "aws" {
   access_key = "my-access-key"
   secret_key = "my-secret-key"
 }
+resource "aws_instance" "practice_ec2" {
+  ami = "xyz"
+  instance_type = "t2.micro"
+  tags = {
+    name = "practice_ec2"
+  }
+}
