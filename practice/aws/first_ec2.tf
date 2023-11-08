72d273bde2e7c369bdcbf4c0d6a831738a039f42
ravikolagani <ravi.kolagani@gmail.com>
2023-11-08 08:27:52 +0800

    github repo created using terraform

--- practice/aws/first_ec2.tf
+++ practice/aws/first_ec2.tf
@@ -1,7 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version ="5.0"
+    }
+  }
+}
 provider "aws" {
   region     = "us-west-2"
   access_key = "my-access-key"
   secret_key = "my-secret-key"
+}
+provider "azurerm" {
+  features {}
 }
 resource "aws_instance" "practice_ec2" {
   ami = "xyz"
